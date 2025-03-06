27ea1430d365c44c7ef8d1e0df3e88cdf5fadbbf
RasmusEng <70199682+RasmusEng@users.noreply.github.com>
2025-03-06 13:30:04 +0100

    Klar

--- Lab4-Proj/Lab4-Proj/Joystick.c
+++ Lab4-Proj/Lab4-Proj/Joystick.c
@@ -4,12 +4,12 @@ int holdJoyStick(Joystick *self, int unUsed) {
 	if (!(PINB & (1 << PB7))) { // DOWN
 		self->held = true;
 		ASYNC(self->gui, freqDown, 0);
-		AFTER(MSEC(250), self, holdJoyStick, 0);
+		AFTER(MSEC(100), self, holdJoyStick, 0);
 	}
 	if (!(PINB & (1 << PB6))) { // UP
 		self->held = true;
 		ASYNC(self->gui, freqUp, 0); 
-		AFTER(MSEC(250), self, holdJoyStick, 0);
+		AFTER(MSEC(100), self, holdJoyStick, 0);
 	}
 	self->held = false;
 	return 0;
@@ -27,11 +27,11 @@ int joyStickHorizontalControll(Joystick *self, int unUsed){
 int joyStickVerticalControll(Joystick *self, int unUsed){
 	if(!self->held){
 	if (!(PINB & (1 << PB7))) { //DOWN
-		ASYNC(self, holdJoyStick, 0);
+		AFTER(MSEC(50), self, holdJoyStick, 0);
 		
 	}
 	if (!(PINB & (1 << PB6))) { //UP
-		ASYNC(self, holdJoyStick, 0);
+		AFTER(MSEC(50), self, holdJoyStick, 0);
 	}
 	if (!(PINB & (1 << PB4))) { //PRESS
 		if (self->risingEdge){
