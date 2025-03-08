0c71b6e0ca955282652902d28d36e314375d3363
Joel Andersson <byggvargen@gmail.com>
2025-03-09 00:48:09 +0100

    :clean:

--- Lab5-ECHO/Lab5-Proj/InteruptHandler.c
+++
@@ -1,9 +0,0 @@
-#include <avr/io.h>
-#include "InteruptHandler.h"
-#include "Joystick.h"
-#include "TinyTimber.h"
-
-
-void Input(InterruptHandler *self, int unUsed) {
-	ASYNC(self->joystick, joyStickVerticalControll, 0);
-}
